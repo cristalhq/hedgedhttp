4fb9313a2387ffc200289fb8992a75f8d05961e7
Oleg Kovalov <oleg@hey.com>
2021-12-07 10:17:02 +0100

    Move stats to a separate file, few small fixes

--- hedged.go
+++ hedged.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -184,7 +183,7 @@ type indexedResp struct {
 	Resp  *http.Response
 }
 
-func reqWithCtx(r *http.Request, ctx context.Context, isHedged bool) (*http.Request, func()) {
+func reqWithCtx(r *http.Request, ctx context.Context, isHedged bool) (*http.Request, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 	if isHedged {
 		ctx = context.WithValue(ctx, hedgedRequest{}, struct{}{})
@@ -199,76 +198,6 @@ type hedgedRequest struct{}
 func IsHedgedRequest(r *http.Request) bool {
 	val := r.Context().Value(hedgedRequest{})
 	return val != nil
-}
-
-// atomicCounter is a false sharing safe counter.
-type atomicCounter struct {
-	count uint64
-	_     [7]uint64
-}
-
-type cacheLine [64]byte
-
-// Stats object that can be queried to obtain certain metrics and get better observability.
-type Stats struct {
-	_                        cacheLine
-	requestedRoundTrips      atomicCounter
-	actualRoundTrips         atomicCounter
-	failedRoundTrips         atomicCounter
-	canceledByUserRoundTrips atomicCounter
-	canceledSubRequests      atomicCounter
-	_                        cacheLine
-}
-
-func (s *Stats) requestedRoundTripsInc()      { atomic.AddUint64(&s.requestedRoundTrips.count, 1) }
-func (s *Stats) actualRoundTripsInc()         { atomic.AddUint64(&s.actualRoundTrips.count, 1) }
-func (s *Stats) failedRoundTripsInc()         { atomic.AddUint64(&s.failedRoundTrips.count, 1) }
-func (s *Stats) canceledByUserRoundTripsInc() { atomic.AddUint64(&s.canceledByUserRoundTrips.count, 1) }
-func (s *Stats) canceledSubRequestsInc()      { atomic.AddUint64(&s.canceledSubRequests.count, 1) }
-
-// RequestedRoundTrips returns count of requests that were requested by client.
-func (s *Stats) RequestedRoundTrips() uint64 {
-	return atomic.LoadUint64(&s.requestedRoundTrips.count)
-}
-
-// ActualRoundTrips returns count of requests that were actually sent.
-func (s *Stats) ActualRoundTrips() uint64 {
-	return atomic.LoadUint64(&s.actualRoundTrips.count)
-}
-
-// FailedRoundTrips returns count of requests that failed.
-func (s *Stats) FailedRoundTrips() uint64 {
-	return atomic.LoadUint64(&s.failedRoundTrips.count)
-}
-
-// CanceledByUserRoundTrips returns count of requests that were canceled by user, using request context.
-func (s *Stats) CanceledByUserRoundTrips() uint64 {
-	return atomic.LoadUint64(&s.canceledByUserRoundTrips.count)
-}
-
-// CanceledSubRequests returns count of hedged sub-requests that were canceled by transport.
-func (s *Stats) CanceledSubRequests() uint64 {
-	return atomic.LoadUint64(&s.canceledSubRequests.count)
-}
-
-// StatsSnapshot is a snapshot of Stats.
-type StatsSnapshot struct {
-	RequestedRoundTrips      uint64 // count of requests that were requested by client
-	ActualRoundTrips         uint64 // count of requests that were actually sent
-	FailedRoundTrips         uint64 // count of requests that failed
-	CanceledByUserRoundTrips uint64 // count of requests that were canceled by user, using request context
-	CanceledSubRequests      uint64 // count of hedged sub-requests that were canceled by transport
-}
-
-// Snapshot of the stats.
-func (s *Stats) Snapshot() StatsSnapshot {
-	return StatsSnapshot{
-		RequestedRoundTrips:      s.RequestedRoundTrips(),
-		ActualRoundTrips:         s.ActualRoundTrips(),
-		FailedRoundTrips:         s.FailedRoundTrips(),
-		CanceledByUserRoundTrips: s.CanceledByUserRoundTrips(),
-		CanceledSubRequests:      s.CanceledSubRequests(),
-	}
 }
 
 var taskQueue = make(chan func())
@@ -359,7 +288,7 @@ func getTimer(duration time.Duration) *time.Timer {
 func returnTimer(timer *time.Timer) {
 	timer.Stop()
 	select {
-	case _ = <-timer.C:
+	case <-timer.C:
 	default:
 	}
 	timerPool.Put(timer)
